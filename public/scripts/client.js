8846d223ecf4fd29d4f349e99bb5c50277a730a8
trevorfunk <trevorjonfunk@gmail.com>
2022-07-20 15:09:48 -0700

    add function to prevent XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//Escape Function
+const escapeText = function (str) {
+ let div = document.createElement("div");
+ div.appendChild(document.createTextNode(str));
+ return div.innerHTML;
+};
+
 const createTweetElement = (tweetInput) => {
+ const safeHTML = escapeText(tweetInput.content.text);
  const tweetTime = timeago.format(tweetInput.created_at);
   const $tweet = $(`<section class="tweet-container">
  <article>
@@ -17,7 +25,7 @@ const createTweetElement = (tweetInput) => {
             <span>${tweetInput.user.handle}</span>
           </div>
         </header>
-        <span class="tweet-content">${tweetInput.content.text}</span>
+        <span class="tweet-content">${safeHTML}</span>
         <footer class="tweet-footer">
           <span class="date">${tweetTime}</span>
           <span class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></i></span>
